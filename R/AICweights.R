d7ff02069065dd9d58f411abee2877214992e240
Korner Fränzi <fraenzi.korner@vogelwarte.ch>
2019-12-05 13:59:06 +0100

    detach the package from the dependendy MuMIn. Changed function AICweights

--- R/AICweights.R
+++ R/AICweights.R
@@ -1,11 +1,7 @@
-AICweights <- function(models){
-  # models: vector of characters with the model names 
-  nmodels <- length(models)
-  AICc_values <- numeric(nmodels)
-  for(i in 1:nmodels) AICc_values[i] <- AICc(eval(parse(text=models[i])))
-  deltaAIC <- AICc_values - min(AICc_values)
+AICweights <- function(AIC_values){
+  # AIC_values: vector with AIC values
+  deltaAIC <- AIC_values - min(AIC_values)
   expdeltaAIC <- exp(-0.5*deltaAIC)
   AICweights <- expdeltaAIC/(sum(expdeltaAIC))
-  names(AICweights) <- models
   return(AICweights)
 }
